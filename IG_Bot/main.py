4637bb628f9745113d2c1a417b6d8748dc8c8efd
chakrastra <rajakvivek080@gmail.com>
2024-08-29 00:14:54 +0530

    Instabot

---
+++ IG_Bot/main.py
@@ -0,0 +1,17 @@
+from instabot import Bot
+
+bot = Bot()
+
+bot.login(username="normiethors",password='#######')
+
+bot.follow("chakrastraa")
+bot.upload_photo("C:/Users/hp/OneDrive/Desktop/Berserk.jpg",caption="...")
+#bot.unfollow("chakrastraa")
+
+bot.send_message("If it is working, Do not touch it",["heisunburger","sidd_3577","abhishek.y.strings"])
+
+followers=bot.get_user_followers("normiethors")
+
+for follower in followers:
+    print(bot.get_user_info(follower))
+    
